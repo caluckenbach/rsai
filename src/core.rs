6f3a8571f28630789688dc9f370544cc5da7d4ce
luckenco <cluckenbach@protonmail.com>
2025-05-14 21:21:09 +0200

    introduce chat completion trait

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,4 @@
 pub mod builder;
 pub mod error;
+pub mod traits;
 pub mod types;
--- src/core/types.rs
+++ src/core/types.rs
@@ -10,3 +10,33 @@ pub enum ChatRole {
     User,
     Assistant,
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ChatCompletionRequest {
+    pub messages: Vec<Message>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ChatCompletionResponse {
+    pub text: String,
+    pub finish_reason: FinishReason,
+    pub usage: LanguageModelUsage,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum FinishReason {
+    Stop,
+    Length,
+    ContentFilter(String),
+    ToolCalls,
+    Error,
+    Other,
+    Unknown,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LanguageModelUsage {
+    pub prompt_tokens: i32,
+    pub completion_tokens: i32,
+    pub total_tokens: i32,
+}
