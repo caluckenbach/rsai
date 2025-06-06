88293626dc1f70a32228bf4db73d1aab332621e8
luckenco <cluckenbach@protonmail.com>
2025-06-06 13:50:12 +0200

    export core modules in lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@ pub mod error;
 pub mod provider;
 
 pub use ai_macros::completion_schema;
+pub use core::{builder::llm, types::{Message, ChatRole}};
+pub use core::builder::ApiKey;
 pub use error::AIError;
