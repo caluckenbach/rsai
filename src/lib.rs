56ad2bbc398bff84f4476cf17286b536292fc553
luckenco <cluckenbach@protonmail.com>
2025-06-26 20:30:46 +0200

    run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod core;
 pub mod provider;
 
-pub use rsai_macros::completion_schema;
 pub use core::builder::ApiKey;
 pub use core::{
     builder::llm,
     types::{ChatRole, Message},
 };
+pub use rsai_macros::completion_schema;
